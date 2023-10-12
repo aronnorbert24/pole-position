54df992e29ffbda8b1cd9bd18367c08993c196f4
Aron Simon <aronnorbert2001@gmail.com>
2023-10-12 11:37:37 +0300

    ITL-3 Fixed Icons

--- src/components/header/PoleHeader.vue
+++ src/components/header/PoleHeader.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="flex">
-    <div>
-      <PolePosition
-        class="m-5 rounded-full border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 computer:scale-110"
+  <div class="flex justify-between mb-16 w-full">
+    <div class="justify-between flex m-5">
+      <PolePositionIcon
+        class="rounded-full bg-white border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 scale-110"
         @click="showHome"
       />
     </div>
-    <div>
+      <div class="justify-between flex">
       <ChampionshipIcon
-        class="m-5 ml-96 rounded-2xl border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 computer:scale-110"
+        class="m-5 rounded-2xl border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 scale-110"
         @click="showPopup"
       />
-    </div>
+      </div>
   </div>
 </template>
 <script setup lang="ts">
-import PolePosition from '../icons/PolePositionIcon.vue'
-import ChampionshipIcon from './ChampionshipIcon.vue'
+import PolePositionIcon from '../icons/PolePositionIcon.vue'
+import ChampionshipIcon from '../icons/ChampionshipIcon.vue'
 
-const emit = defineEmits(['showHome'])
+const emit = defineEmits(['showHome', 'showPopup'])
 
 function showHome() {
   emit('showHome')
+}
+
+function showPopup() {
+  emit('showPopup')
 }
 </script>
