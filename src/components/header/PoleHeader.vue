09b060d70abd098e8245c6acec70f23063c6ff5a
Aron Simon <aronnorbert2001@gmail.com>
2023-10-12 10:14:07 +0300

    ITL-3 Trying to fix icons

--- src/components/header/PoleHeader.vue
+++ src/components/header/PoleHeader.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="flex h-56 w-full">
-    <PolePosition
-      class="m-5 rounded-full border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 computer:scale-110"
-      @click="showHome"
-    />
-    <Championship
-      class="m-5 ml-auto rounded-full border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 computer:scale-110"
-      @click="showPopup"
-    />
+  <div class="flex">
+    <div>
+      <PolePosition
+        class="m-5 rounded-full border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 computer:scale-110"
+        @click="showHome"
+      />
+    </div>
+    <div>
+      <ChampionshipIcon
+        class="m-5 ml-96 rounded-2xl border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 computer:scale-110"
+        @click="showPopup"
+      />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import PolePosition from '../icons/PolePositionIcon.vue'
-import Championship from '../icons/ChampionshipIcon.vue'
+import ChampionshipIcon from './ChampionshipIcon.vue'
 
 const emit = defineEmits(['showHome'])
 
