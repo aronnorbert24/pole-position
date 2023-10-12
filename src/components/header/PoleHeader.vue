07a72a877628092cdf6aabc1a90c8a1a2e9b6131
Aron Simon <aronnorbert2001@gmail.com>
2023-10-12 10:13:37 +0300

    ITL-3 Trying to fix icons

--- src/components/header/PoleHeader.vue
+++ src/components/header/PoleHeader.vue
@@ -4,10 +4,15 @@
       class="m-5 rounded-full border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 computer:scale-110"
       @click="showHome"
     />
+    <Championship
+      class="m-5 ml-auto rounded-full border-2 border-red-600 transition-transform duration-300 ease-in-out hover:scale-125 hover:cursor-pointer phone:scale-100 computer:scale-110"
+      @click="showPopup"
+    />
   </div>
 </template>
 <script setup lang="ts">
 import PolePosition from '../icons/PolePositionIcon.vue'
+import Championship from '../icons/ChampionshipIcon.vue'
 
 const emit = defineEmits(['showHome'])
 
