8200b66b0793c3cb98d45cb3f28ffe967c7d4e1f
Aron Simon <aronnorbert2001@gmail.com>
2023-10-25 12:22:46 +0300

    ITL-30 Create the design for the username

--- src/components/articles/ArticleComment.vue
+++ src/components/articles/ArticleComment.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="mt-5 h-fit w-full bg-white p-2">
-    <img class="h-16 w-16 rounded-full border-2 border-red-600" :src="user.userPicture" />
+  <div class="mt-5 flex h-fit w-full bg-white p-2">
+    <div class="flex">
+      <img class="h-16 w-16 rounded-full border-2 border-red-600" :src="user.userPicture" />
+    </div>
+    <div class="ml-4 flex">
+      <p class="mt-2 text-lg font-semibold text-black">{{ user.username }}</p>
+    </div>
   </div>
 </template>
 
