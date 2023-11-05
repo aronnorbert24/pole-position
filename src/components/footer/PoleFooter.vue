43fb00cfcace231cc06e07e0fcff9c46f6f2ee9a
Aron Simon <aronnorbert2001@gmail.com>
2023-11-05 15:08:59 +0200

    ITL-41 Create the footer design for mobile

--- src/components/footer/PoleFooter.vue
+++ src/components/footer/PoleFooter.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="mt-36 w-full bg-red-600 px-16 pb-4 align-bottom">
+  <div class="mt-36 w-full bg-red-600 px-4 pb-4 align-bottom computer:px-16">
     <div class="flex justify-between">
       <div
         class="flex transition-transform duration-300 ease-in-out hover:cursor-pointer"
         @click="showArticlesByCategory('F1')"
       >
-        <p class="mt-6 text-lg font-semibold text-white">F1</p>
-        <F1Icon class="ml-4 bg-white" />
+        <p class="mt-6 font-semibold text-white phone:hidden computer:text-lg">F1</p>
+        <F1Icon class="ml-4 bg-white phone:ml-8" />
       </div>
       <div
         class="flex transition-transform duration-300 ease-in-out hover:cursor-pointer"
         @click="showArticlesByCategory('F2')"
       >
-        <p class="mt-6 text-lg font-semibold text-white">F2</p>
+        <p class="mt-6 font-semibold text-white phone:hidden computer:text-lg">F2</p>
         <F2Icon class="ml-4 bg-white" />
       </div>
       <div
         class="flex transition-transform duration-300 ease-in-out hover:cursor-pointer"
         @click="showArticlesByCategory('F3')"
       >
-        <p class="mt-6 text-lg font-semibold text-white">F3</p>
-        <F3Icon class="ml-4 bg-white" />
+        <p class="mt-6 font-semibold text-white phone:hidden computer:text-lg">F3</p>
+        <F3Icon class="ml-4 bg-white phone:mr-10" />
       </div>
     </div>
     <div class="mt-10 flex justify-between">
@@ -28,15 +28,15 @@
         class="flex transition-transform duration-300 ease-in-out hover:cursor-pointer"
         @click="showArticlesByCategory('WEC')"
       >
-        <p class="mt-6 text-lg font-semibold text-white">WEC</p>
+        <p class="mt-6 font-semibold text-white phone:hidden computer:text-lg">WEC</p>
         <WecIcon class="ml-4 bg-white" />
       </div>
       <div
         class="flex transition-transform duration-300 ease-in-out hover:cursor-pointer"
         @click="showArticlesByCategory('MotoGP')"
       >
-        <p class="mt-6 text-lg font-semibold text-white">MotoGP</p>
-        <MotogpIcon class="ml-4 bg-white" />
+        <p class="mt-6 font-semibold text-white phone:hidden computer:text-lg">MotoGP</p>
+        <MotogpIcon class="ml-4 bg-white phone:mr-10" />
       </div>
     </div>
   </div>
