73f4a84f27217ee0fee9f401a8d9b24d7832e633
Aron <aronnorbert2001@gmail.com>
2023-11-29 15:36:37 +0200

    ITL-62 Create a server.ts file

---
+++ server/src/server.ts
@@ -0,0 +1,30 @@
+import express, { Response } from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+const { database, connectionString, port } = require('./dotenv.config')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+dotenv.config()
+
+mongoose
+  .connect(connectionString, {
+    retryWrites: true,
+    w: 'majority',
+    dbName: database,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB database ' + mongoose.connection.name)
+  })
+  .catch((error: any) => {
+    console.error('Failed to connect to MongoDB database', error)
+  })
+
+app.get('/', function (res: Response) {
+  res.send('Hey Bro!')
+})
+
+app.listen(port, () => console.log(port))
